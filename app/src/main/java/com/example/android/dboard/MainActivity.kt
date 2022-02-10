9ba8ba370ec34ff974d8d22571e6b318f5a88324
sartra <strenshaw@gmail.com>
2022-02-10 18:15:33 -0500

    spacebar - add image?

--- app/src/main/java/com/example/android/dboard/MainActivity.kt
+++ app/src/main/java/com/example/android/dboard/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.dboard
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
@@ -100,7 +103,16 @@ fun DboardButton(
             .padding(2.dp),
         onClick = model.callback
     ) {
-        Text(textAlign = TextAlign.Center, text = model.char.toString(), color= Color.White)
+        if(model.type == DboardButtonType.Char) {
+            Text(textAlign = TextAlign.Center, text = model.char.toString(), color= Color.White)
+        } else if (model.type == DboardButtonType.Space) {
+            SpaceBarButton()
+//            Icon(
+//                imageVector = Icons.Filled.Clear,
+//                contentDescription = "Localized description",
+//                modifier = Modifier.padding(end = 8.dp)
+//            )
+        }
     }
 }
 
