fd64360b6510da6db396b70c4d3b61692ce054cb
sartra <strenshaw@gmail.com>
2022-02-17 12:29:48 -0500

    compose test

---
+++ app/src/androidTest/java/com/example/android/dboard/organisms/DboardKtTest.kt
@@ -0,0 +1,16 @@
+package com.example.android.dboard.organisms
+
+import junit.framework.TestCase
+
+class DboardKtTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testDboard() {}
+
+    fun testHandleButtonClick() {}
+}
